8357c20652248fd8e51aea683d9bdbfa101fae31
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-22 16:49:19 +0900

    Invalidating Cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,10 +26,9 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        $books = $books->get();
 
-        $casheKey = 'books:' . $filter . ':' . $title;
-        $books = cache()->remember($cacheKey, 'books', 3600, fn() => $books->get());
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -55,14 +54,13 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn($query) => $query->latest()
-                ])
-            ]
-        );
+        $cacheKey = 'book:' . $book->id;
+
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+            'reviews' => fn($query) => $query->latest()
+        ]));
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
