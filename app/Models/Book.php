6288255ebd49dc202711d2288c49eca0a4830c26
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-22 13:26:51 +0900

    Created a resource controller

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -12,5 +14,37 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withCount([
+            'reviews' => fn (Builder $query, $from = null, $to = null) => $this->dateRangerFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder|QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn (Builder $query) => $this->dateRangerFilter($q, $from, $to)
+            ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangerFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } else if (!$from && $to) {
+            $query->where('created_at', '<=', $from);
+        } else if ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
