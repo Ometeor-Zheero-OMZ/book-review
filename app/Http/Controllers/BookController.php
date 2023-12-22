48c36af3a284a5a423f8a036ab4fc745b4fbee65
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2023-12-22 15:52:27 +0900

    Filter Popular or Highest Rated

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
@@ -13,12 +13,20 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
         $books = Book::when(
             $title,
-            fn ($query, $title) => $query->title($title)
-        )
-            ->get();
+            fn($query, $title) => $query->title($title)
+        );
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
